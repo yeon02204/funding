b08a45d12d4725cecd6074f113b598390ca6f03c
yeon02204 <yeon02204@gmail.com>
2026-03-26 17:36:49 +0900

    fix: force JVM timezone to Asia/Seoul

--- src/main/java/com/funding/funding/FundingApplication.java
+++ src/main/java/com/funding/funding/FundingApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling // Spring이 Scheduled를 스캔하고 실행하도록 켜는 스위치
 public class FundingApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(FundingApplication.class, args);
 	}
 
